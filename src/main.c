d0a2f41af637e0de0e8ed1b149953f1d6001b665
lostnut <lostnut@pm.me>
2018-12-06 19:23:18 +0100

    Implement simple evolution for U-235

--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
+#include <math.h>
 #include <stdio.h>
-int main(void) {
-        printf("Hello world!\n");
+
+int main(void)
+{
+        const double half_life = 2.2195e+16;
+        const double lambda    = log(2) / half_life;
+        printf("Enter the initial proportion of U-235\n");
+        double prop;
+        scanf("%lf", &prop);
+        printf("Enter the time of evolution\n");
+        double time;
+        scanf("%lf", &time);
+        printf("The final proportion of U-235 is %f\n",
+               prop * exp(-lambda * time));
         return 0;
 }
