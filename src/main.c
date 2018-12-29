b0e11ab6af6826788574a9ebeb46fa640be4852e
lostnut <lostnut@pm.me>
2018-12-29 13:54:18 +0100

    Back to simple Hello world in main

--- src/main.c
+++ src/main.c
@@ -1,17 +1,8 @@
 #include <math.h>
 #include <stdio.h>
+#include "die.h"
 
 int main(void)
 {
-        const double half_life = 2.2195e+16;
-        const double lambda    = log(2) / half_life;
-        printf("Enter the initial proportion of U-235\n");
-        double prop;
-        scanf("%lf", &prop);
-        printf("Enter the time of evolution\n");
-        double time;
-        scanf("%lf", &time);
-        printf("The final proportion of U-235 is %f\n",
-               prop * exp(-lambda * time));
-        return 0;
+        printf("Hello world!\n");
 }
